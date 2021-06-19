c5459bf77886660564975763e5fc870fe5a44804
zucc31801111 <48320549+zucc31801111@users.noreply.github.com>
2021-06-19 11:30:42 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 // const mongoose = require('mongoose');
 require('dotenv/config');
-// const postRoute = require('./routers/posts');
+const postRoute = require('./routers/posts');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql')
@@ -10,34 +10,16 @@ const mysql = require('mysql')
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false}));
-// app.use('/posts',postRoute);
+app.use('/posts',postRoute);
 
-const connection = mysql.createConnection({
-    host:'localhost',           //数据库地址
-    user: 'root',               //用户名
-    password: '1234',           //密码
-    port : '3306',              //端口
-    database: 'user',           //库名
-    multipleStatements:true     //允许执行多条语句
-})
-app.get('/',(req,res) => {
-    // res.send('we are on home');
-    const sql ='SELECT * FROM user' //user为表名
-    connection.query(sql,(err,results) =>{
-        if(err){
-            return res.json({
-                code: 1,
-                message: '用户不存在',
-                affextedRows: 0
-            })
-        }
-        res.json ({
-            code : 200,
-            data: results,
-            affextedRows:results.affextedRows
-        })
-    })
+
+app.all("*", function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", " 3.2.1");
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
 });
-
 
 app.listen(3000);
--- routers/posts.js
+++ routers/posts.js
@@ -1,54 +1,75 @@
 const express = require('express');
 const router = express.Router();
-// const Post = require('../modules/Post');
+const mysql = require('mysql')
 
-router.get('/', async (req,res) => {
-    try{
-        const posts = await Post.find();
-        res.json(posts);
-    }catch(err){
-        res.json({message: err});
-    }
-});
-
-router.post('/', async (req,res) => {
-    const post = new Post({
-        title: req.body.title,
-        description: req.body.description
-    });
-    try{
-    const savedPost = await post.save()
-    res.json(savedPost);
-    }catch(err){
-        res.json({message: err});
-    }
-});
-
-router.get('/:postId', async (req,res) => {
-    try{
-        const post = await Post.findById(req.params.postId);
-        res.json(post);
-    }catch(err){
-        res.json({message: err});
-    }
-});
-
-router.delete('/:postId', async (req,res) => {
-    try{
-        const removedPost = await Post.remove({_id: req.params.postId});
-        res.json(removedPost);
-    }catch(err){
-        res.json({message: err});
-    }
+const connection = mysql.createConnection({
+    host:'localhost',           //数据库地址
+    user: 'root',               //用户名
+    password: '1234',           //密码
+    port : '3306',              //端口
+    database: 'user',           //库名
+    multipleStatements:true     //允许执行多条语句
 })
 
-router.patch('/:postId', async (req,res) => {
-    try{
-        const updatePost = await Post.updateOne({_id: req.params.postId}, {$set: {title: req.body.title}});
-        res.json(updatePost);
-    }catch(err){
-        res.json({message: err});
-    }
-})
+connection.connect();
 
+router.post('/addNews', async (req,res,next) => {
+    let addform = req.body;
+    let newsTitle = addform.newsTitle
+    let newsAuthor = addform.newsAuthor
+    let newsMsg = addform.newsMsg
+    let newsDate = addform.newsDate
+    let newsImg = addform.newsImg
+    let newsTitleImg = addform.newsTitleImg
+    console.log(typeof newsDate)
+    console.log(addform)
+    let sql = "insert into news(news_title,news_author,news_msg,news_img,news_title_img) values(?,?,?,?,?)"
+        await connection.query(sql, [newsTitle,newsAuthor,newsMsg,newsImg,newsTitleImg] ,(err,result)=>{
+            if(err){
+                return res.json({message: err,code: 1, msg: '添加信息失败'});
+            }
+            res.send({code: 200, msg: '添加信息成功',data: result});  
+        }); 
+});
+
+
+router.post('/addUsers', async (req,res,next) => {
+    let addform = req.body;
+    let user_name = addform.user_name
+    let user_password = addform.user_password
+    let user_count = addform.user_count
+    console.log(addform)
+        let sql = "select * from user where user_count = ?"
+        console.log(sql)
+            await connection.query(sql, [user_count],  (err,result)=>{
+            console.log(result)
+            if(err){
+                return  res.json({code: 1, msg: '查询失败'})
+            }
+            if(result===null){
+                  return  res.json({code: 1, msg: 'yicuncai',data: result})
+              }
+              else{
+                sql = "insert into user(user_name,user_password,user_count) values(?,?,?)"
+                console.log(sql)
+                connection.query(sql,[user_name,user_password,user_count],  (err,results2)=>{
+                    console.log(results2)
+                    if(err){
+                        return res.json({message: err,code: 1, msg: '添加信息失败'});
+                    }
+                    res.json({code: 200, msg: '添加信息成功',data: results2})
+                })  
+              }  
+            })                  
+});
+
+router.get('/allNews', async (req,res) => {   
+        const sql = 'SELECT * FROM news' 
+        await connection.query(sql,(err,results)=>{
+            if(err){
+                return res.json({message: err,code: 1, msg: '查询信息失败'});
+            }
+            res.json({code: 200, msg: '查询信息成功',data: results});
+        })
+});
 module.exports = router;
